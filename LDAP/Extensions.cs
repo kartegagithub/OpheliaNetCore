5d8b8535067d1ab164f950295d664b9b31d720d3
Fatih Çelik <fatih.celik@kartega.com>
2020-06-01 22:12:17 +0300

    LDAP additional check

--- LDAP/Extensions.cs
+++ LDAP/Extensions.cs
@@ -8,6 +8,10 @@ namespace Ophelia.LDAP
     {
         public static object GetPropertyValue(this SearchResult result, string key)
         {
+            if (result == null)
+                return "";
+            if (result.Properties == null)
+                return "";
             if (result.Properties.Contains(key) && result.Properties[key].Count > 0)
                 return result.Properties[key][0];
             return "";
