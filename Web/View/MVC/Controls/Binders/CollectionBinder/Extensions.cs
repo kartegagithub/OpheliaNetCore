5ec620a870af9299e0fc8bf591be68c4f7c2a111
Fatih Çelik <fatih.celik@kartega.com>
2020-06-21 20:07:24 +0300

    Rename

--- Web/View/MVC/Controls/Binders/CollectionBinder/Extensions.cs
+++ Web/View/MVC/Controls/Binders/CollectionBinder/Extensions.cs
@@ -121,7 +121,7 @@ namespace Ophelia.Web.View.Mvc.Controls.Binders.CollectionBinder
             return (Columns.EnumColumn<TModel, T>)binder.AddColumn(column);
         }
 
-        public static Columns.TextColumn<TModel, T> AddEditableNumericColumn<TModel, T>(this CollectionBinder<TModel, T> binder, Expression<Func<T, object>> expression, Expression<Func<T, object>> identifierExpression, string identifierKeyword = "", string Text = "", object htmlAttributes = null) where T : class where TModel : ListModel<T>
+        public static Columns.NumericColumn<TModel, T> AddEditableNumericColumn<TModel, T>(this CollectionBinder<TModel, T> binder, Expression<Func<T, object>> expression, Expression<Func<T, object>> identifierExpression, string identifierKeyword = "", string Text = "", object htmlAttributes = null) where T : class where TModel : ListModel<T>
         {
             var column = new Columns.NumericColumn<TModel, T>(binder, Text);
             column.Expression = expression;
@@ -130,7 +130,7 @@ namespace Ophelia.Web.View.Mvc.Controls.Binders.CollectionBinder
             column.HtmlAttributes = htmlAttributes;
             column.Text = Text;
             column.AllowEdit = true;
-            return (Columns.TextColumn<TModel, T>)binder.AddColumn(column);
+            return (Columns.NumericColumn<TModel, T>)binder.AddColumn(column);
         }
         public static Columns.NumericColumn<TModel, T> AddNumericColumn<TModel, T>(this CollectionBinder<TModel, T> binder, Expression<Func<T, object>> expression) where T : class where TModel : ListModel<T> { return binder.AddNumericColumn(expression, ""); }
         public static Columns.NumericColumn<TModel, T> AddNumericColumn<TModel, T>(this CollectionBinder<TModel, T> binder, Expression<Func<T, object>> expression, string Text) where T : class where TModel : ListModel<T> { return binder.AddNumericColumn(expression, Text, true); }
