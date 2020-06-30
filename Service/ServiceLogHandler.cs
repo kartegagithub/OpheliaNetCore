ca3fe715637df88481b575b3a181ee71cd97ce66
Fatih Çelik <fatih.celik@kartega.com>
2020-06-30 07:58:07 +0300

    Exception handleing in service log handler

--- Service/ServiceLogHandler.cs
+++ Service/ServiceLogHandler.cs
@@ -21,36 +21,44 @@ namespace Ophelia.Service
         }
         protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            var requestInfo = string.Format("{0} {1}", request.Method, request.RequestUri);
-
-            var requestMessage = new byte[0];
-            var headers = "";
-            if (request.Content != null)
+            HttpResponseMessage response;
+            try
             {
-                requestMessage = request.Content.ReadAsByteArrayAsync().Result;
+                var requestInfo = string.Format("{0} {1}", request.Method, request.RequestUri);
+
+                var requestMessage = new byte[0];
+                var headers = "";
+                if (request.Content != null)
+                {
+                    requestMessage = request.Content.ReadAsByteArrayAsync().Result;
+                }
+                if (request.Headers != null)
+                {
+                    headers = request.Headers.ToJson();
+                }
+                this.LogRequest(0, requestInfo, Encoding.UTF8.GetString(requestMessage), headers);
+
+                response = base.SendAsync(request, cancellationToken).Result;
+                var httpStatus = (int)response.StatusCode;
+
+                byte[] responseMessage;
+
+                if (response.Content != null)
+                {
+                    responseMessage = response.Content.ReadAsByteArrayAsync().Result;
+                }
+                else
+                {
+                    responseMessage = Encoding.UTF8.GetBytes(response.ReasonPhrase);
+                }
+
+                this.LogResponse(httpStatus, requestInfo, responseMessage);
             }
-            if (request.Headers != null)
+            catch (System.Exception ex)
             {
-                headers = request.Headers.ToJson();
+                new ServiceObjectResult<bool>().Fail(ex);
+                response = new HttpResponseMessage(System.Net.HttpStatusCode.InternalServerError) { Content = new StringContent(new { error = true, message = ex.ToString() }.ToJson()) };
             }
-            this.LogRequest(0, requestInfo, Encoding.UTF8.GetString(requestMessage), headers);
-
-            var response = base.SendAsync(request, cancellationToken).Result;
-            var httpStatus = (int)response.StatusCode;
-
-            byte[] responseMessage;
-
-            if (response.Content != null)
-            {
-                responseMessage = response.Content.ReadAsByteArrayAsync().Result;
-            }
-            else
-            {
-                responseMessage = Encoding.UTF8.GetBytes(response.ReasonPhrase);
-            }
-
-            this.LogResponse(httpStatus, requestInfo, responseMessage);
-
             return Task<HttpResponseMessage>.Factory.StartNew(() => response);
         }
 
