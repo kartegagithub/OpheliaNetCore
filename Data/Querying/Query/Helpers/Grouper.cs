2e19429f26686778c4307a010b461fe9633a63f5
Fatih Çelik <fatih.celik@kartega.com>
2020-12-01 16:59:44 +0300

    Added using

--- Data/Querying/Query/Helpers/Grouper.cs
+++ Data/Querying/Query/Helpers/Grouper.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.Internal;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Runtime.Serialization;
