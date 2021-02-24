e4151e41b9adb102d069577bb3ea49935c0fdcb7
Fatih Çelik <fatih.celik@kartega.com>
2021-02-24 17:46:21 +0300

    I18N Integration

---
+++ Integration/I18NService/Models/TranslationPool_i18n.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Ophelia.Integration.I18NService.Models
+{
+    public class TranslationPool_i18n: IDisposable
+    {
+        public string LanguageCode { get; set; }
+
+        public string Description { get; set; }
+
+        public void Dispose()
+        {
+            this.LanguageCode = "";
+            this.Description = "";
+        }
+    }
+}
