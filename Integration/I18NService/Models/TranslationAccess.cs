584a0c12dded25d8ce6018f89fda11afcf7b08d6
Fatih Çelik <fatih.celik@kartega.com>
2021-02-25 12:54:30 +0300

    Category added to translation

--- Integration/I18NService/Models/TranslationAccess.cs
+++ Integration/I18NService/Models/TranslationAccess.cs
@@ -7,11 +7,13 @@ namespace Ophelia.Integration.I18NService.Models
     public class TranslationAccess: IDisposable
     {
         public string Name { get; set; }
+        public string CategoryCode { get; set; }
         public int Count { get; set; }
 
         public void Dispose()
         {
             this.Name = "";
+            this.CategoryCode = "";
         }
     }
 }
