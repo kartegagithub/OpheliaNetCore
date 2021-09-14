e3e95fe758c40ec5165e65200c0c39b05ce05f6a
Fatih Çelik <github@kartega.com>
2021-09-14 12:35:41 +0300

    ToNullable added to lists

--- Extensions/NullableExtension.cs
+++ Extensions/NullableExtension.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Ophelia
 {
@@ -7,6 +9,22 @@ namespace Ophelia
         public static Nullable<T> AsNullable<T>(this T value) where T : struct
         {
             return new Nullable<T>(value);
+        }
+        public static List<long?> ToNullable(this List<long> value)
+        {
+            return (from item in value select new Nullable<long>(item)).ToList();
+        }
+        public static List<int?> ToNullable(this List<int> value)
+        {
+            return (from item in value select new Nullable<int>(item)).ToList();
+        }
+        public static List<decimal?> ToNullable(this List<decimal> value)
+        {
+            return (from item in value select new Nullable<decimal>(item)).ToList();
+        }
+        public static List<double?> ToNullable(this List<double> value)
+        {
+            return (from item in value select new Nullable<double>(item)).ToList();
         }
     }
 }
