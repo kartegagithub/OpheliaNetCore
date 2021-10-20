74036872fceabea569ef044e3b4d15dc559b4d84
Fatih Çelik <github@kartega.com>
2021-10-20 10:57:28 +0300

    BookStack integration

---
+++ Integration/Documentation/BookStack/Models/TagModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ophelia.Integration.Documentation.BookStack.Models
+{
+    public class TagModel
+    {
+        public string name { get; set; }
+        public string value { get; set; }
+        public int order { get; set; }
+    }
+}
