1c4f48291b8b46c75225b438e2d08a2b574384e4
Ali Bekdaş <ali.bekdas@kartega.com>
2021-11-23 11:19:10 +0300

    Hava durumu entegrasyonu

---
+++ Integration/Weather/WeatherService.cs
@@ -0,0 +1,79 @@
+﻿using Ophelia.Service;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace Ophelia.Integration.Weather
+{
+    /// <summary>
+    /// Kullanılan Site: https://www.weatherapi.com/docs/
+    /// </summary>
+    public class WeatherService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ServiceURL"></param>
+        /// <param name="APIKey"></param>
+        public WeatherService(string ServiceURL, string APIKey)
+        {
+            this.ServiceURL = ServiceURL;
+            this.APIKey = APIKey;
+        }
+
+        /// <summary>
+        /// Servis Linki
+        /// </summary>
+        public string ServiceURL { get; set; }
+
+        /// <summary>
+        /// Serviste kullanılacak API Key
+        /// </summary>
+        public string APIKey { get; set; }
+
+        /// <summary>
+        /// Girilen bilgilere göre anlık hava durumu bilgisi döner. İl (İlçe) veya enlem & boylam girmek zorunludur.
+        /// </summary>
+        /// <param name="city">İl/İlçe</param>
+        /// <param name="lang">Dil. Örn: tr Varsayılan olarak İngilizcedir</param>
+        /// <param name="latitude">Enlem</param>
+        /// <param name="longitude">Boylam</param>
+        public ServiceObjectResult<WeatherResult> CurrentWeather(string city, string lang, string latitude, string longitude)
+        {
+            var result = new ServiceObjectResult<WeatherResult>();
+            try
+            {
+                if (!string.IsNullOrEmpty(lang))
+                    lang = $"&lang={lang}";
+
+                var q = "";
+                if (!string.IsNullOrEmpty(city))
+                {
+                    q = $"&q={city}";
+                }
+                else if (!string.IsNullOrEmpty(latitude) && !string.IsNullOrEmpty(longitude))
+                {
+                    q = $"&q={latitude},{longitude}";
+                }
+                else
+                {
+                    result.Fail("CityOrCoordinateCanNotBeBlank");
+                    return result;
+                }
+
+                var URL = $"{this.ServiceURL}?key={this.APIKey}{q}{lang}";
+
+                var serviceResult = URL.DownloadURL();
+                if (!string.IsNullOrEmpty(serviceResult))
+                    result.SetData(serviceResult.FromJson<WeatherResult>());
+                else
+                    result.Fail("AProblemOccurred");
+            }
+            catch (Exception ex)
+            {
+                result.Fail(ex);
+            }
+            return result;
+        }
+    }
+}
