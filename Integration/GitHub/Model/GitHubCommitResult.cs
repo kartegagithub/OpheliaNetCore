f59911807ba3aa60aeace037768fb1dd9c6ad3b0
busrasekerr <83954852+busrasekerr@users.noreply.github.com>
2022-01-12 15:20:47 +0300

    GitHub; Useless code lines removed from Github models.

--- Integration/GitHub/Model/GitHubCommitResult.cs
+++ Integration/GitHub/Model/GitHubCommitResult.cs
@@ -7,57 +7,38 @@ namespace Ophelia.Integration.GitHub
     [DataContract(IsReference = true)]
     public class GitHubCommitResult
     {
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
         [DataMember]
         [JsonProperty("sha")]
         public string Sha { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("node_id")]
         public string NodeID { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("commit")]
         public Commit Commit { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("url")]
-        public string Url { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+        public string Url { get; set; }      
+        
         [DataMember]
         [JsonProperty("html_url")]
         public string HtmlUrl { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("comments_url")]
         public string CommentsUrl { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("author")]
         public MainResultAuthorCommitter Author { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("committer")]
         public MainResultAuthorCommitter Committer { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("parents")]
         public Parent[] Parents { get; set; }
@@ -65,111 +46,73 @@ namespace Ophelia.Integration.GitHub
 
     public class MainResultAuthorCommitter
     {
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
         [DataMember]
         [JsonProperty("login")]
         public string Login { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("id")]
         public int ID { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
-        [DataMember]
+
         [JsonProperty("node_id")]
         public string NodeID { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("avatar_url")]
         public string AvatarUrl { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("gravatar_id")]
         public string GravatarID { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("url")]
         public string Url { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("html_url")]
         public string HtmlUrl { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("followers_url")]
         public string FollowersUrl { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("following_url")]
         public string FollowingUrl { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("gists_url")]
         public string GistsUrl { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("starred_url")]
         public string StarredUrl { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("subscriptions_url")]
         public string SubscriptionsUrl { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("organizations_url")]
         public string OrganizationsUrl { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("repos_url")]
         public string ReposUrl { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("events_url")]
         public string EventsUrl { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("received_events_url")]
         public string ReceivedEventsUrl { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("type")]
         public string Type { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("site_admin")]
         public bool SiteAdmin { get; set; }
@@ -177,45 +120,30 @@ namespace Ophelia.Integration.GitHub
 
     public class Commit
     {
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
         [DataMember]
         [JsonProperty("author")]
         public CommitAuthorAndCommitter Author { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+        
         [DataMember]
         [JsonProperty("committer")]
         public CommitAuthorAndCommitter Committer { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+        
         [DataMember]
         [JsonProperty("message")]
         public string Message { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+        
         [DataMember]
         [JsonProperty("tree")]
         public Tree Tree { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+        
         [DataMember]
         [JsonProperty("url")]
         public string Url { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+        
         [DataMember]
         [JsonProperty("comment_count")]
         public int CommentCount { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+        
         [DataMember]
         [JsonProperty("verification")]
         public Verification Verification { get; set; }
@@ -223,21 +151,14 @@ namespace Ophelia.Integration.GitHub
 
     public class CommitAuthorAndCommitter
     {
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
         [DataMember]
         [JsonProperty("name")]
         public string Name { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("email")]
         public string Email { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("date")]
         public DateTime Date { get; set; }
@@ -245,15 +166,10 @@ namespace Ophelia.Integration.GitHub
 
     public class Tree
     {
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
         [DataMember]
         [JsonProperty("sha")]
         public string Sha { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+        
         [DataMember]
         [JsonProperty("url")]
         public string Url { get; set; }
@@ -261,27 +177,18 @@ namespace Ophelia.Integration.GitHub
 
     public class Verification
     {
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
         [DataMember]
         [JsonProperty("verified")]
         public bool Verified { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+        
         [DataMember]
         [JsonProperty("reason")]
         public string Reason { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("signature")]
         public object Signature { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+
         [DataMember]
         [JsonProperty("payload")]
         public object Payload { get; set; }
@@ -289,21 +196,14 @@ namespace Ophelia.Integration.GitHub
 
     public class Parent
     {
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
         [DataMember]
         [JsonProperty("sha")]
         public string Sha { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+        
         [DataMember]
         [JsonProperty("url")]
         public string Url { get; set; }
-        /// <summary>
-        /// Lokasyon Bilgileri
-        /// </summary>
+        
         [DataMember]
         [JsonProperty("html_url")]
         public string HtmlUrl { get; set; }
