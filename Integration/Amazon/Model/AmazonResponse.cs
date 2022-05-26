5399ffc24b180699ef8965263d3203f2a626b1cb
mehmetkulle <76935592+mehmetkulle@users.noreply.github.com>
2022-05-26 14:45:44 +0300

    AmazonS3 Entegrasyonu

---
+++ Integration/Amazon/Model/AmazonResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ophelia.Integration.Amazon.Model
+{
+    public class AmazonResponse
+    {
+        public string URL { get; set; }
+        public DateTime ExpireDate { get; set; }
+    }
+}
