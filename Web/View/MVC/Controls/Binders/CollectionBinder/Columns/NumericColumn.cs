381c913db70547f397ff916c9bd31bea34a6bd6a
Fatih Çelik <github@kartega.com>
2022-06-04 14:48:16 +0300

    CollectionBinder NumericColumn display revision

--- Web/View/MVC/Controls/Binders/CollectionBinder/Columns/NumericColumn.cs
+++ Web/View/MVC/Controls/Binders/CollectionBinder/Columns/NumericColumn.cs
@@ -9,9 +9,12 @@ namespace Ophelia.Web.View.Mvc.Controls.Binders.CollectionBinder.Columns
         public string Format { get; set; }
         public override object GetValue(T item)
         {
+            this.SetFormat(this.Format, this.Binder.Configuration.DecimalFormat, this.Binder.Configuration.IntFormat);
+
             var value = base.GetValue(item);
             if (value == null)
                 return 0.ToString(this.Format);
+            
             if (string.IsNullOrEmpty(this.Format))
                 return value;
             else
@@ -33,6 +36,36 @@ namespace Ophelia.Web.View.Mvc.Controls.Binders.CollectionBinder.Columns
             textbox.Value = Convert.ToString(this.GetValue(entity));
             this.SetAttributes(textbox);
             return textbox;
+        }
+        public void SetFormat(string format, string defaultDecimalFormat, string defaultIntFormat)
+        {
+            if (format == "-")
+                return;
+            if (!string.IsNullOrEmpty(format))
+                this.Format = format;
+            else
+            {
+                if (!string.IsNullOrEmpty(defaultDecimalFormat) || !string.IsNullOrEmpty(defaultIntFormat))
+                {
+                    var propType = this.Expression.GetPropertyType();
+                    if (propType == null)
+                        return;
+
+                    if (!string.IsNullOrEmpty(defaultDecimalFormat) && propType.Name.IndexOf("Decimal", StringComparison.InvariantCultureIgnoreCase) > -1)
+                    {
+                        this.Format = defaultDecimalFormat;
+                    }
+                    else if (!string.IsNullOrEmpty(defaultDecimalFormat) &&
+                        (propType.Name.IndexOf("long", StringComparison.InvariantCultureIgnoreCase) > -1
+                        || propType.Name.IndexOf("int", StringComparison.InvariantCultureIgnoreCase) > -1
+                        || propType.Name.IndexOf("int16", StringComparison.InvariantCultureIgnoreCase) > -1
+                        || propType.Name.IndexOf("int32", StringComparison.InvariantCultureIgnoreCase) > -1
+                        || propType.Name.IndexOf("int64", StringComparison.InvariantCultureIgnoreCase) > -1))
+                    {
+                        this.Format = defaultIntFormat;
+                    }
+                }
+            }
         }
         public NumericColumn(CollectionBinder<TModel, T> binder, string Name) : base(binder, Name)
         {
