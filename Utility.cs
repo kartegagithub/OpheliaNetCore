d43d220acc6005c6b08068937249baaf072f7273
pinarakdagg <pinar.akdag@kartega.com>
2022-12-05 09:21:40 +0300

    Generarate function added.

--- Utility.cs
+++ Utility.cs
@@ -4,10 +4,67 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
+using IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Collections.Generic;
 namespace Ophelia
 {
     public static class Utility
     {
+        public static string Generate(PasswordOptions opts = null)
+        {
+            if (opts == null) opts = new PasswordOptions()
+            {
+                RequiredLength = 8,
+                RequiredUniqueChars = 4,
+                RequireDigit = true,
+                RequireLowercase = true,
+                RequireNonAlphanumeric = true,
+                RequireUppercase = true
+            };
+
+            string[] randomChars = new[] {
+                "ABCDEFGHJKLMNOPQRSTUVWXYZ",
+                "abcdefghijkmnopqrstuvwxyz",
+                "0123456789",
+                "!@$?_-"
+            };
+            CryptoRandom rand = new CryptoRandom();
+            List<char> chars = new List<char>();
+
+            if (opts.RequireUppercase)
+                chars.Insert(rand.Next(0, chars.Count), randomChars[0][rand.Next(0, randomChars[0].Length)]);
+
+            if (opts.RequireLowercase)
+                chars.Insert(rand.Next(0, chars.Count), randomChars[1][rand.Next(0, randomChars[1].Length)]);
+
+            if (opts.RequireDigit)
+                chars.Insert(rand.Next(0, chars.Count), randomChars[2][rand.Next(0, randomChars[2].Length)]);
+
+            if (opts.RequireNonAlphanumeric)
+                chars.Insert(rand.Next(0, chars.Count), randomChars[3][rand.Next(0, randomChars[3].Length)]);
+
+            var randomCharList = new List<string>();
+            if (opts.RequireUppercase)
+                randomCharList.Add("ABCDEFGHJKLMNOPQRSTUVWXYZ");
+            if (opts.RequireLowercase)
+                randomCharList.Add("abcdefghijkmnopqrstuvwxyz");
+            if (opts.RequireDigit)
+                randomCharList.Add("0123456789");
+            if (opts.RequireNonAlphanumeric)
+                randomCharList.Add("!@$?_-");
+
+            randomChars = randomCharList.ToArray();
+
+            for (int i = chars.Count; i < opts.RequiredLength || chars.Distinct().Count() < opts.RequiredUniqueChars; i++)
+            {
+                string rcs = randomChars[rand.Next(0, randomChars.Length)];
+                chars.Insert(rand.Next(0, chars.Count), rcs[rand.Next(0, rcs.Length)]);
+            }
+
+            return new string(chars.ToArray());
+        }
         public static string GenerateRandomPassword(int maxSize, bool isNumeric = false)
         {
             char[] chars = new char[62];
