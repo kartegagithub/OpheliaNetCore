7272706a251cac7394d6b644be8abbca3bdb02c6
Fatih Çelik <github@kartega.com>
2023-01-19 17:38:02 +0300

    Sessionless Captcha revisions

---
+++ Web/View/MVC/ActionFilters/CaptchaValidationAttribute.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Ophelia.Web.View.Mvc.Html;
+using Ophelia.Web.View.Mvc.Controllers.Base;
+
+namespace Ophelia.Web.View.Mvc.ActionFilters
+{
+    public class CaptchaValidationAttribute : ActionFilterAttribute
+    {
+        public bool AlwaysShow { get; set; }
+        public int ErrorCount { get; set; }
+        public string EncKey { get; set; }
+        public CaptchaValidationAttribute()
+        {
+            
+        }
+        public override void OnActionExecuting(ActionExecutingContext actionContext)
+        {
+            var request = actionContext.HttpContext.Request;
+            var captcha = new Captcha();
+            captcha.CaptchaModel.AlwaysShow = AlwaysShow;
+            captcha.CaptchaModel.ErrorCount = ErrorCount;
+            captcha.EncKey = this.EncKey;
+            if (captcha.Enabled())
+            {
+                var InputCaptcha = request.Form["CaptchaCode"];
+                var CaptchaSessionKey = request.Form["CKey"];
+                if (!captcha.CheckCaptcha(InputCaptcha, CaptchaSessionKey))
+                    ((Microsoft.AspNetCore.Mvc.Controller)actionContext.Controller).ModelState.AddModelError("CaptchaError", captcha.CaptchaModel.AlertMessage.Message);
+            }
+        }
+    }
+}
