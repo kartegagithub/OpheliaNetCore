b55d9ea4e987e3c5e75e7b8409d9475cc79becff
Kartega Yazılım ve Danışmanlık A.Ş <56034514+kartegagithub@users.noreply.github.com>
2023-10-11 10:01:18 +0300

    TranslationAccess revision

--- Integration/I18NService/Models/TranslationAccess.cs
+++ Integration/I18NService/Models/TranslationAccess.cs
@@ -7,6 +7,7 @@ namespace Ophelia.Integration.I18NService.Models
     public class TranslationAccess: IDisposable
     {
         public string Name { get; set; }
+        public string ProjectCode { get; set; }
         public string CategoryCode { get; set; }
         public int Count { get; set; }
 
