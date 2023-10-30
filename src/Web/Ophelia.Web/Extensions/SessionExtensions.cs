9f79284ae2b67b2ca50ec215512a5c28ada9ddd3
Kartega Yazılım ve Danışmanlık A.Ş <56034514+kartegagithub@users.noreply.github.com>
2023-10-30 15:06:30 +0300

    7.0.0.28 Session SetObject JsonSerializerSettings added

--- src/Web/Ophelia.Web/Extensions/SessionExtensions.cs
+++ src/Web/Ophelia.Web/Extensions/SessionExtensions.cs
@@ -7,7 +7,16 @@ namespace Ophelia
     {
         public static void SetObject(this ISession session, string key, object value)
         {
-            session.SetString(key, JsonConvert.SerializeObject(value));
+            session.SetObject(key, value, new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                NullValueHandling = NullValueHandling.Ignore
+            });
+        }
+
+        public static void SetObject(this ISession session, string key, object value, JsonSerializerSettings settings)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value, settings));
         }
 
         public static T GetObject<T>(this ISession session, string key)
