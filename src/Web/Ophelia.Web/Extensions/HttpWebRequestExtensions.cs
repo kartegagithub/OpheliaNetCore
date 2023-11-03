b6553f10d2a0a79c2cf6bc786c812e0dd054970f
fatihCinarKrtg <63067360+fatihCinarKrtg@users.noreply.github.com>
2023-11-03 10:10:25 +0300

    Health-Hes Integration and WebRequestExtensions added.

---
+++ src/Web/Ophelia.Web/Extensions/HttpWebRequestExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+
+namespace Ophelia.Web
+{
+    public static class HttpWebRequestExtensions
+    {
+        public static HttpWebResponse GetResponseWithoutException(this HttpWebRequest req)
+        {
+            try
+            {
+                return (HttpWebResponse)req.GetResponse();
+            }
+            catch (WebException we)
+            {
+                var resp = we.Response as HttpWebResponse;
+                if (resp == null)
+                    throw;
+                return resp;
+            }
+        }
+    }
+}
