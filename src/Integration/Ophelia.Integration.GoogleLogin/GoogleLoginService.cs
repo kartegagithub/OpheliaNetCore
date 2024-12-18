3dff7e178802fafca926797131d7b1d37ac25777
Ali Bekdaş <ali.bekdas@kartega.com>
2024-12-18 17:12:56 +0300

    google login

---
+++ src/Integration/Ophelia.Integration.GoogleLogin/GoogleLoginService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Google.Apis.Auth;
+using Ophelia;
+using static Google.Apis.Auth.GoogleJsonWebSignature;
+
+namespace Ophelia.Integration.GoogleAuth
+{
+    public class GoogleLoginService
+    {
+        public GoogleLoginService(string clientID)
+        {
+            ClientId = clientID;
+        }
+
+        private string ClientId = "";
+
+        public Payload ValidateGoogleToken(string token)
+        {
+            ValidationSettings? settings = new GoogleJsonWebSignature.ValidationSettings()
+            {
+                Audience = new List<string>() { ClientId }
+            };
+            Payload payload = GoogleJsonWebSignature.ValidateAsync(token, settings).Result;
+
+            if (payload == null)
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            var expirationDate = DateTimeOffset.FromUnixTimeSeconds(payload.ExpirationTimeSeconds.Value).LocalDateTime;
+
+            if (DateTime.Now > expirationDate)
+            {
+                throw new Exception("The token has expired.");
+            }
+
+            return payload;
+        }
+    }
+}
