f7f9dad4ba0f9fd56f55bec05fea999b611a454a
Kartega Yazılım ve Danışmanlık A.Ş. <56034514+kartegagithub@users.noreply.github.com>
2025-11-02 19:03:08 +0300

    Conversion bugfix

--- src/Core/Ophelia/Extensions/ObjectExtensions.cs
+++ src/Core/Ophelia/Extensions/ObjectExtensions.cs
@@ -1,33 +1,70 @@
-﻿namespace Ophelia
+﻿using System;
+
+namespace Ophelia
 {
     public static class ObjectExtensions
     {
         public static short ToShort(this object value)
         {
-            return value.ToInt16();
+            short result = 0;
+            if (value != null && short.TryParse(value.ToString(), out result))
+                return result;
+            return result;
         }
-        public static short ToInt16(this object value)
+        public static Int16 ToInt16(this object value)
         {
-            return value != null ? value.GetType().ToInt16(value) : (short)0;
+            Int16 result = 0;
+            if (value != null && Int16.TryParse(value.ToString(), out result))
+                return result;
+            return result;
         }
 
-        public static int ToInt32(this object value)
+        public static Int32 ToInt32(this object value)
         {
-            return value != null ? value.GetType().ToInt32(value) : 0;
+            Int32 result = 0;
+            if (value != null && Int32.TryParse(value.ToString(), out result))
+                return result;
+            return result;
         }
 
         public static long ToInt64(this object value)
         {
-            return value != null ? value.GetType().ToInt64(value) : 0;
+            Int64 result = 0;
+            if (Int64.TryParse(value.ToString(), out result))
+                return result;
+            return result;
         }
 
         public static byte ToByte(this object value)
         {
-            return value != null ? value.GetType().ToByte(value) : (byte)0;
+            byte result = 0;
+            if (value != null && byte.TryParse(value.ToString(), out result))
+                return result;
+            return result;
         }
         public static bool ToBoolean(this object value)
         {
-            return value != null ? value.GetType().ToBoolean(value) : false;
+            if (value != null)
+            {
+                var typeCode = Type.GetTypeCode(value.GetType());
+                switch (typeCode)
+                {
+                    case TypeCode.Boolean:
+                        return (bool)value;
+                    case TypeCode.Char:
+                        var cValue = (char)value;
+                        return cValue.Equals("Y") || cValue.Equals("y");
+                    case TypeCode.String:
+                        var sValue = value.ToString().ToLowerInvariant();
+                        if (string.IsNullOrEmpty(sValue))
+                            return false;
+
+                        return value.Equals("true") || value.Equals("yes");
+                    default:
+                        return value.ToInt64() > 0;
+                }
+            }
+            return false;
         }
     }
 }
