b88bc7b0910b177c667bb30efaaee8f9c24df87d
Kartega Yazılım ve Danışmanlık A.Ş. <56034514+kartegagithub@users.noreply.github.com>
2025-11-28 09:58:47 +0300

    8.0.0.170 ToJson FromJson parametrized

--- src/Core/Ophelia/Extensions/JsonExtensions.cs
+++ src/Core/Ophelia/Extensions/JsonExtensions.cs
@@ -4,21 +4,59 @@ namespace Ophelia
 {
     public static class JsonExtensions
     {
-        public static string ToJson(this object obj, int recursionDepth)
+        private static JsonSerializerSettings _toJsondefaultSettings;
+        private static JsonSerializerSettings _fromJsondefaultSettings;
+        public static void SetFromJsonDefaultSettings(JsonSerializerSettings settings)
+        {
+            _fromJsondefaultSettings = settings;
+        }
+        public static void SetToJsonDefaultSettings(JsonSerializerSettings settings)
+        {
+            _toJsondefaultSettings = settings;
+        }
+        public static string ToJson(this object obj, JsonSerializerSettings settings = null)
         {
             if (obj != null)
-                return JsonConvert.SerializeObject(obj, Formatting.None, new JsonSerializerSettings() { ReferenceLoopHandling = ReferenceLoopHandling.Ignore, MaxDepth = recursionDepth, NullValueHandling = NullValueHandling.Ignore });
+            {
+                if (settings == null)
+                {
+                    if (_toJsondefaultSettings != null)
+                    {
+                        settings = _toJsondefaultSettings;
+                    }
+                    else
+                    {
+                        settings = new JsonSerializerSettings()
+                        {
+                            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                            MaxDepth = 3,
+                            NullValueHandling = NullValueHandling.Ignore
+                        };
+                    }
+                }
+                return JsonConvert.SerializeObject(obj, Formatting.None, settings);
+            }
             return string.Empty;
         }
-        public static string ToJson(this object obj)
+        public static T FromJson<T>(this string value, JsonSerializerSettings settings = null)
         {
-            if (obj != null)
-                return JsonConvert.SerializeObject(obj, Formatting.None, new JsonSerializerSettings() { ReferenceLoopHandling = ReferenceLoopHandling.Ignore, NullValueHandling = NullValueHandling.Ignore });
-            return string.Empty;
-        }
-        public static T FromJson<T>(this string value)
-        {
-            return JsonConvert.DeserializeObject<T>(value, new JsonSerializerSettings() { ReferenceLoopHandling = ReferenceLoopHandling.Ignore, NullValueHandling = NullValueHandling.Ignore });
+            if (settings == null)
+            {
+                if (_fromJsondefaultSettings != null)
+                {
+                    settings = _fromJsondefaultSettings;
+                }
+                else
+                {
+                    settings = new JsonSerializerSettings()
+                    {
+                        ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                        MaxDepth = 3,
+                        NullValueHandling = NullValueHandling.Ignore
+                    };
+                }
+            }
+            return JsonConvert.DeserializeObject<T>(value, settings);
         }
     }
 }
